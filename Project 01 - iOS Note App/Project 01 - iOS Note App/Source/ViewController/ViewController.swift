135ee7584776c165b2c6a324a8ef98ad8c969b0e
minhee <xwoud@naver.com>
2020-08-21 15:06:34 +0900

    [UPDATE] 프로젝트 생성

---
+++ Project 01 - iOS Note App/Project 01 - iOS Note App/Source/ViewController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 01 - iOS Note App
+//
+//  Created by 김민희 on 2020/08/21.
+//  Copyright © 2020 김민희. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
